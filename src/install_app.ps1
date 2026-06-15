5d6d94fdf8d6fc5e4186af5058ebacd24a906995
RioRichard <thanh.huuphungak47@gmail.com>
2026-06-15 12:51:28 +0700

    fix variable name

--- src/install_app.ps1
+++ src/install_app.ps1
@@ -10,12 +10,14 @@ function Install-App
   try
   {
     
-    $Command = @("install" ,"$PackageName ","--accept-package-agreements ","--accept-source-agreements ","-h ")
+    $Command = @("install","-e","--id","$PackageName ","--accept-package-agreements ","--accept-source-agreements ","-h ")
     if ([String]::IsNullOrWhiteSpace($Custom))
     {
       $Command += "--custom /configure $Custom"
     }
-    winget @Command
+
+    Write-Host "winget $Command"
+    winget $Command
   } catch
   {
     Write-Error "Error when install: $Name"
